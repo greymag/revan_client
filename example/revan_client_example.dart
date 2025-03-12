4682094632465b9a40bc86771ec6d817f87277dd
greymag <greymag@gmail.com>
2025-03-12 18:12:35 +0100

    Example

--- example/revan_client_example.dart
+++ example/revan_client_example.dart
@@ -1,3 +1,15 @@
-import 'package:revan_client/revan_client.dart';
+import 'package:revan_client/src/revan_api.dart';
 
-void main() {}
+Future<void> main() async {
+  final api = RevanApi(apiUrl: 'http://127.0.0.1:8090');
+  final res = await api.trubar.find('lahko');
+
+  if (res.isValue) {
+    final data = res.asValue!.value;
+    print("Success! Result:\n\t${data.list.join("\n\t")}");
+  } else {
+    print("Error: ${res.asError!.error}");
+  }
+
+  api.dispose();
+}
